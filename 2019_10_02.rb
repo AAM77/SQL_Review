a870d6e39b146387cc497ff0d19f5c1b5ec4f1ba
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-02 16:07:00 -0400

    practice GROUP BY

--- 2019_10_02.rb
+++ 2019_10_02.rb
@@ -106,3 +106,40 @@ WHERE
 title LIKE '%Truman%'
 OR
 title LIKE '%truman%';
+
+
+--- ROUND & AVG ----------------------------
+SELECT ROUND(AVG(amount),2) FROM payment;
+
+--- MIN ------------------------------------
+SELECT MIN(amount) FROM payment;
+
+--- MAX ------------------------------------
+SELECT MAX(amount) FROM payment;
+
+--- SUM ------------------------------------
+SELECT SUM(amount) FROM payment;
+
+--- ROUND ----------------------------------
+SELECT ROUND(SUM(amount), 1) FROM payment;
+
+---- GROUP BY ------------------------------
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC;
+
+
+SELECT staff_id, COUNT(payment_id)
+FROM payment
+GROUP BY staff_id;
+
+
+SELECT rating, COUNT(rating)
+FROM film
+GROUP BY rating;
+
+
+SELECT rental_duration, COUNT(rental_duration)
+FROM film
+GROUP BY rental_duration;
