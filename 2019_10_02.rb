24d53ebbb7aef58e99e3ab5388b7fa2e45b5eb08
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-02 13:14:13 -0400

    practice the IN operator

--- 2019_10_02.rb
+++ 2019_10_02.rb
@@ -6,3 +6,22 @@
 
 -- SELECT amount, payment_date FROM payment
 -- WHERE payment_date NOT BETWEEN '2007-02-07' AND '2007-02-15';
+
+
+SELECT customer_id, rental_id, return_date
+FROM rental
+WHERE customer_id IN (7, 10, 13)
+ORDER BY return_date DESC;
+
+-- similar to --
+
+SELECT customer_id, rental_id, return_date
+FROM rental
+WHERE customer_id = 7
+OR customer_id = 13
+OR customer_id = 10
+ORDER BY return_date DESC;
+
+-- other examples:
+SELECT * FROM payment
+WHERE amount IN (7.99, 8.99);
