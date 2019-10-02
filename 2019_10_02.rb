096fe5529d98f95bbb243ae0c01e7d90fc2e8b3f
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-10-02 13:48:48 -0400

    practice challenges using previously practiced concepts

--- 2019_10_02.rb
+++ 2019_10_02.rb
@@ -49,3 +49,60 @@ WHERE first_name LIKE '_her%';
 SELECT first_name, last_name
 FROM customer
 WHERE first_name NOT LIKE 'Jen%';
+
+
+--------------------------------------------------------
+
+SELECT COUNT(amount)
+FROM payment
+WHERE amount > 5;
+
+--------------------------------------------------------
+
+SELECT COUNT(first_name)
+FROM actor
+WHERE first_name LIKE 'P%' OR first_name LIKE 'p%';
+
+--------------------------------------------------------
+
+SELECT COUNT(DISTINCT district) from address;
+
+--------------------------------------------------------
+
+SELECT DISTINCT district from address
+ORDER BY district;
+
+--------------------------------------------------------
+
+SELECT COUNT(*)
+FROM film
+WHERE rating = 'R'
+AND replacement_cost BETWEEN 5 AND 15;
+
+---------------------------------------------------------
+
+
+SELECT title
+FROM film
+WHERE
+title LIKE 'Truman%'
+OR
+title LIKE 'truman%'
+OR
+title LIKE '%Truman'
+OR
+title LIKE '%truman'
+OR
+title LIKE '%Truman%'
+OR
+title LIKE '%truman%';
+
+
+-------------------SAME AS--------------------------------------
+
+SELECT COUNT(title)
+FROM film
+WHERE
+title LIKE '%Truman%'
+OR
+title LIKE '%truman%';
